17a11389fac892c0d437a980ae0eaf2ba153c1d3
nicolas lavoine <nlavoine.dev@gmail.com>
2019-05-06 13:49:50 +0200

    Added GoogleMap Plugin

--- src/components/SuppliersMap.vue
+++ src/components/SuppliersMap.vue
@@ -1,8 +1,17 @@
 <template>
-    <h1>Carte des fournisseurs</h1>
+    <div class="container">
+        <h1>Carte des fournisseurs</h1>
+        <<gmap-map
+            :center="{lat:48.864716, lng:2.349014}"
+            :zoom="7"
+            style="width: 100%; height: 800px"
+    >
+    </gmap-map>
+    </div>
 </template>
 
 <script>
+
     export default {
         name: "SuppliersMap.vue"
     }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDgQRHUsMFvtkiM91rlD3TJKNyumP0tSn8',
+  }
+})
 
 
 Vue.config.productionTip = false
@@ -11,14 +17,14 @@ Vue.config.productionTip = false
 new Vue({
   router,
   render: h => h(App),
-  methodes:{
+  /*methodes:{
     onSuppliersListClick:function(){
 
     },
     onMapClick:function(){
 
     }
-  },
+  },*/
   /*components: {
     'SuppliersList': SuppliersList,
     'SuppliersMap': SuppliersMap

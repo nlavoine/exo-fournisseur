64f9e035030f4811f74f7f46b3efae43346d061d
nicolas lavoine <nlavoine.dev@gmail.com>
2019-05-07 11:06:18 +0200

    Added axios.get working

--- src/components/SuppliersList.vue
+++ src/components/SuppliersList.vue
@@ -5,7 +5,7 @@
                 <div class="card" style="width: 18rem; margin: 0 auto">
                     <div class="card-body">
                         <p class="card-text">{{ item.name }}</p>
-                        <router-link  :to="{name:'supplier', params: {id:item.id, name:item.name, status:item.status, checkedAt:item.checkedAt} }" class="btn btn-primary">{{ item.name }}</router-link>
+                        <router-link :to="{name:'supplier', params: {id:item.id, name:item.name, status:item.status, checkedAt:item.checkedAt} }" class="btn btn-primary">{{ item.name }}</router-link>
                     </div>
                 </div>
             </li>
@@ -19,6 +19,7 @@
 
     //var options = {weekday: "long", year: "numeric", month: "long", day: "numeric", hour:"2-digit", minute:"2-digit"};
     var date = new Date()
+    const axios = require('axios');
 
     export default {
         name: "SuppliersList.vue",
@@ -37,8 +38,31 @@
                         status: false,
                         checkedAt: format(date, 'fr-FR', '2019-01-01')
                     }
-                ]
+                ],
+                loading: false,
+                error: null,
             }
+        },
+        created: async function () {
+
+            axios.get('https://api-suppliers.herokuapp.com/api/suppliers',
+                {
+                    onDownloadProgress: progressEvent => {
+                        console.log(progressEvent.loaded);
+                    }
+                })
+                .then(function (response) {
+                    // handle success
+                    console.log("My success: " + response);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log("My error: " + error);
+                })
+                .then(function () {
+                        // always executed
+                    }
+                );
         }
     }
 </script>
